275903758603d53a5e9002bdbc202002ce1311f0
Victor Augusto De Aquino Silvério <victoraugusto4096@outlook.com>
2025-08-16 02:18:24 -0300

    Iniciando a analise via pandas

--- Semana_2/Atividade-Analisando_o_famoso_dataset_Iris.py
+++ Semana_2/Atividade-Analisando_o_famoso_dataset_Iris.py
@@ -1,1 +1,23 @@
-print("Senai")
+'''
+Nome: Victor Augusto
+
+Atividade: Analisando o famoso dataset Iris
+Data Ínicio: 16/08 - 02:12
+
+Objetivos: 
+
+Qual é a média (por espécie) de cada medida (sepal/petal, comprimento/largura)?
+Qual é o desvio padrão de cada variável por espécie?
+Quantas observações existem por espécie? (contagem de instâncias)
+Qual é o valor mínimo e máximo (amplitude) das medidas por espécie para saber a dispersão?
+Qual é a média geral (sem agrupamento) das variáveis — comparando contexto geral e por espécie?
+
+'''
+
+import pandas as pd
+
+path = r'C:\Users\victo\Python_Senai390\Semana_2\iris.csv'
+
+df = pd.read_csv(path)
+
+print(df.head())
