a36b9e45b31abd9a4e33b64307f70ba504b73511
Victor Augusto De Aquino Silvério <victoraugusto4096@outlook.com>
2025-09-19 06:55:15 -0300

    Iniciando semana 6 (web scrapy & analise)

---
+++ Semana_6/livraria/livraria/spiders/catalogo_spider.py
@@ -0,0 +1,22 @@
+#Spider para extrair dados de livros do site books.toscrape.com.
+
+import scrapy
+
+class CatalogoSpider(scrapy.Spider):
+    name = 'catalogo'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        # Itera sobre cada livro
+        for book in response.css('article.product_pod'):
+            yield {
+                'titulo': book.css('h3 a::attr(title)').get(),
+                'preco': book.css('.price_color::text').get(),
+                'disponibilidade': book.css('.availability::text').get().strip()
+            }
+        
+        # Encontra o link e segue
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
