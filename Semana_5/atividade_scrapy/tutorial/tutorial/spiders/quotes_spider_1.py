0cf39aa0f8e2958fd5babefc6b2c46a5ca05fb2b
Victor Augusto De Aquino Silvério <victoraugusto4096@outlook.com>
2025-09-19 06:35:26 -0300

    Tutorial_scrapy-spider

---
+++ Semana_5/atividade_scrapy/tutorial/tutorial/spiders/quotes_spider_1.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        "https://quotes.toscrape.com/page/1/",
+        "https://quotes.toscrape.com/page/2/",
+    ]
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"quotes-{page}.html"
+        Path(filename).write_bytes(response.body)
